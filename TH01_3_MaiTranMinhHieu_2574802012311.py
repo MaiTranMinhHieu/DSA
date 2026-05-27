3a1e080930bda09f98ecc2b3343ef9c1b5489481
MaiTranMinhHieu <mfb5nv68yj@privaterelay.appleid.com>
2026-05-27 22:12:21 +0700

    Delete TH01_3_MaiTranMinhHieu_2574802012311.py

--- TH01_3_MaiTranMinhHieu_2574802012311.py
+++
@@ -1,9 +0,0 @@
-def linearsearch(arr,key):
-    for i in range(len(arr)):
-        if arr[i]==key:
-            return i
-    return -1
-arr=['Bao','An','Dat','Duc','Hung','Phi','Vinh','Dung']
-key='Phi'
-res=linearsearch(arr,key)
-print("vi tri tim thay thu i la: "+str(res))
